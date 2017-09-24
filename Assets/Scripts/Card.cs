2c1c90129da111c7296570888d7cab3bc83c0860
Wesley Pickering <wesley.pickering@berkeley.edu>
2017-09-23 17:50:21 -0700

    First Commit -wes

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,79 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour {
+
+	public static bool STOP_MECH = false;
+
+	[SerializeField] private int _state;
+	[SerializeField] private int _cardValue;
+	[SerializeField] private bool _initialized = false;
+
+	private Sprite cardBack;
+	private Sprite cardFace;
+	private GameObject gameManager;
+
+	void Start(){
+		_state = 1;
+		gameManager = GameObject.FindGameObjectWithTag ("Manager");
+	}
+
+	public void setUpCards() {
+		cardBack = gameManager.GetComponent<ConcentrationManager> ().getCardBack ();
+		cardFace = gameManager.GetComponent<ConcentrationManager> ().getCardFace (_cardValue);
+
+		StartCoroutine (showCards ());
+	}
+
+	IEnumerator showCards() {
+		_state = 1;
+		yield return new WaitForSeconds (10);
+		flipCard ();
+	}
+
+	public void flipCard(){
+		if (_state == 0) {
+			_state = 1;
+		} else if (_state == 1) {
+			_state = 0;
+		}
+
+		if (_state == 0 && !STOP_MECH) {
+			GetComponent<Image> ().sprite = cardBack;
+		} else if (_state == 1 && !STOP_MECH) {
+			GetComponent<Image> ().sprite = cardFace;
+		}
+	}
+
+	public int cardValue {
+		get { return _cardValue; }
+		set { _cardValue = value; }
+	}
+
+	public int state{
+		get { return _state; }
+		set { _state = value; }
+	}
+		
+	public bool initialized {
+		get { return _initialized; }
+		set { _initialized = value; }
+	}
+
+	public void falsecheck(){
+		StartCoroutine (pause ());
+	}
+
+	IEnumerator pause() {
+		yield return new WaitForSeconds(1);
+		if (_state == 0) {
+			GetComponent<Image> ().sprite = cardBack;
+		} else if (state == 1) {
+			GetComponent<Image> ().sprite = cardFace;
+		}
+		STOP_MECH = false;
+	}
+}
---
+++ Assets/Scripts/ConcentrationManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ConcentrationManager: MonoBehaviour {
+
+	public Sprite[] cardFace ;
+	public Sprite cardBack;
+	public GameObject[] cards;
+	public Text matchText;
+
+	private bool init = false;
+	private int matches = 13;
+
+	void Update() {
+		if (!init)
+			initializeCards ();
+
+		if (Input.GetMouseButtonUp(0)) {
+			checkCards ();
+		}
+	}
+
+	void initializeCards (){
+		for (int i = 0; i < 2; i++) {
+			for (int j = 1; j < 14; j++) {
+				bool test = false;
+				int choice = 0;
+				while (!test) {
+					choice = Random.Range (0, cards.Length);
+					test = !(cards [choice].GetComponent<Card> ().initialized);
+				}
+				cards [choice].GetComponent<Card> ().cardValue = j;
+				cards [choice].GetComponent<Card> ().initialized = true;
+			}
+		}
+		foreach (GameObject c in cards) {
+			c.GetComponent<Card> ().setUpCards ();
+		}
+
+		if (!init) {
+			init = true;
+		}
+	}
+
+	public Sprite getCardBack() {
+		return cardBack;
+	}
+
+	public Sprite getCardFace(int i) {
+		return cardFace [i-1];
+	}
+
+	void checkCards() {
+		List<int> c = new List<int> ();
+		for (int i = 0; i < cards.Length; i++) {
+			if (cards[i].GetComponent<Card>().state == 1) {
+				c.Add (i);
+			}
+		}
+		if (c.Count == 2) {
+			cardComparison (c);
+		}
+	}
+
+	void cardComparison ( List<int> c) {
+		Card.STOP_MECH = true;
+
+		int x = 0;
+
+		if (cards[c[0]].GetComponent<Card>().cardValue == cards[c[1]].GetComponent<Card>().cardValue) {
+			x = 2;
+//			matches--;
+//			matchText.text = "Number of Matches: " + matches;
+
+			if (matches == 0) {
+				SceneManager.LoadScene ("Menu");
+			}
+		}
+
+		for (int i = 0; i < c.Count; i++) {
+			cards [c [i]].GetComponent<Card> ().state = x;
+			cards [c [i]].GetComponent<Card> ().falsecheck ();
+		}
+	}
+}
